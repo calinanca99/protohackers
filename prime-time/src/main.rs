70d5d7adfd4ef8eb04f2c242d7d55cd4a2376e98
calinanca99 <calinanca99@gmail.com>
2023-05-06 18:12:32 +0200

    Add logger

--- prime-time/src/main.rs
+++ prime-time/src/main.rs
@@ -1,3 +1,5 @@
+use env_logger::Env;
+use log::{debug, info};
 use primes::is_prime;
 use serde_json::{json, Value};
 use std::{
@@ -9,6 +11,8 @@ use utils::addr;
 
 fn handle_malformed_request(stream: &mut TcpStream) {
     let malformed_response = json!({"result": "failure"}).to_string();
+    debug!("Malformed response: {:?}", malformed_response);
+    info!("Sending malformed response...");
     let _ = stream
         .write(malformed_response.as_bytes())
         .expect("Cannot write to TCP stream");
@@ -23,12 +27,15 @@ fn format_response(is_prime: bool) -> String {
 }
 
 fn handle_connection(stream: &mut TcpStream) {
+    info!("Established connection with: {:?}", stream.local_addr());
+
     let mut buffer = String::new();
     let _ = stream
         .read_to_string(&mut buffer)
         .expect("Cannot read TCP stream");
 
     let request: serde_json::Result<Value> = serde_json::from_str(&buffer);
+    debug!("Payload: {:?}", request);
     if request.is_err() {
         handle_malformed_request(stream);
         return;
@@ -50,6 +57,7 @@ fn handle_connection(stream: &mut TcpStream) {
     }
 
     // At this point it's known that `number` is a valid JSON number
+    debug!("Checking if {:?} is prime.", number);
     let is_prime: bool = if number.is_f64() {
         false
     } else if number.is_i64() {
@@ -65,15 +73,23 @@ fn handle_connection(stream: &mut TcpStream) {
         is_prime(number.as_u64().unwrap())
     };
 
+    let response = format_response(is_prime);
+    debug!("Response: {:?}", response);
+    info!("Sending response...");
+
     let _ = stream
-        .write(format_response(is_prime).as_bytes())
+        .write(response.as_bytes())
         .expect("Cannot write to TCP stream");
 }
 
 fn main() -> std::io::Result<()> {
+    let env = Env::new().filter_or("LOG_LEVEL", "debug");
+
+    env_logger::init_from_env(env);
+
     let addr = addr();
     let listener = TcpListener::bind(addr)?;
-    println!("Server listening on: {}", addr);
+    info!("Server listening on: {}", addr);
 
     for mut stream in listener.incoming().flatten() {
         thread::spawn(move || handle_connection(&mut stream));
