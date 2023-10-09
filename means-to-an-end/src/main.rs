6bc280c44092dfa009e817e9947879fb287fe2f8
calinanca99 <calinanca99@gmail.com>
2023-10-09 18:02:26 +0200

    Define the data model

--- means-to-an-end/src/main.rs
+++ means-to-an-end/src/main.rs
@@ -1,7 +1,47 @@
-use std::net::{TcpListener, TcpStream};
+#![allow(dead_code)]
 
+use std::{
+    collections::BTreeMap,
+    net::{TcpListener, TcpStream},
+};
+
+use env_logger::Env;
 use log::{error, info};
 use utils::addr;
+
+struct InsertMessage {
+    /// Number of seconds since the UNIX Epoch.
+    timestamp: Timestamp,
+    /// Price of the asset in pennies.
+    price: Price,
+}
+
+struct QueryMessage {
+    /// Earliest timestamp of the period.
+    min_time: Timestamp,
+    /// Latest timestamp of the period.
+    max_time: Timestamp,
+}
+
+enum Request {
+    Insert(InsertMessage),
+    Query(QueryMessage),
+}
+
+struct Response {
+    /// Represents the mean of the inserted prices with timestamps T, where
+    /// `min_time` <= T <= `max_time`. If there are no samples, then the `mean`
+    /// is 0.
+    ///
+    /// The `mean` is rounded down in case it's not an integer.
+    mean: Price,
+}
+
+type Timestamp = i32;
+type Price = i32;
+
+/// Represents the prices associated with a session.
+type SessionPrices = BTreeMap<Timestamp, Price>;
 
 fn handle_connection(connection: TcpStream) {
     info!("Established connection with: {:?}", connection.peer_addr());
@@ -10,7 +50,11 @@ fn handle_connection(connection: TcpStream) {
 }
 
 fn main() {
+    let env = Env::new().filter_or("LOG_LEVEL", "debug");
+    env_logger::init_from_env(env);
+
     let listener = TcpListener::bind(addr()).expect("Cannot bind to address");
+    info!("Started listening on: {:?}", listener);
 
     for stream in listener.incoming() {
         match stream {
