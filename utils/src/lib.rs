69609357fec0f25940005fe67a0f4e68a8371356
calinanca99 <calinanca99@gmail.com>
2024-02-16 10:04:15 +0100

    Handle case when `ENV` var is lowercase

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -2,7 +2,7 @@ use std::env;
 
 pub fn addr() -> &'static str {
     match env::var("ENV") {
-        Ok(var) if var == "PROTO" => "0.0.0.0:8000",
+        Ok(var) if var.to_lowercase() == "proto" => "0.0.0.0:8000",
         Ok(_) | Err(_) => "127.0.0.1:8000",
     }
 }
