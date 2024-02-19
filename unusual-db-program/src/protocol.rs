5feee844016de53c9b8b7e6463c3c719d1842c1c
Calin Anca <calinanca99@gmail.com>
2024-02-19 16:07:44 +0100

    Add unit tests

--- unusual-db-program/src/protocol.rs
+++ unusual-db-program/src/protocol.rs
@@ -1,5 +1,6 @@
 pub const VERSION_SPECIAL_KEY: &str = "version";
 
+#[derive(Debug, PartialEq)]
 pub enum Message {
     Insert(InsertMessage),
     Retrieve(RetrieveMessage),
@@ -23,11 +24,95 @@ impl Message {
     }
 }
 
+#[derive(Debug, PartialEq)]
 pub struct InsertMessage {
     pub key: String,
     pub value: String,
 }
 
+#[derive(Debug, PartialEq)]
 pub struct RetrieveMessage {
     pub key: String,
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::protocol::*;
+
+    #[test]
+    fn parses_insert_with_one_equal() {
+        let m = Message::new("foo=bar".to_string());
+        assert_eq!(
+            Message::Insert(InsertMessage {
+                key: "foo".to_string(),
+                value: "bar".to_string()
+            }),
+            m.unwrap()
+        );
+    }
+
+    #[test]
+    fn parses_insert_with_two_equals() {
+        let m = Message::new("foo=bar=baz".to_string());
+        assert_eq!(
+            Message::Insert(InsertMessage {
+                key: "foo".to_string(),
+                value: "bar=baz".to_string()
+            }),
+            m.unwrap()
+        );
+    }
+
+    #[test]
+    fn parses_insert_with_empty_value() {
+        let m = Message::new("foo=".to_string());
+        assert_eq!(
+            Message::Insert(InsertMessage {
+                key: "foo".to_string(),
+                value: "".to_string()
+            }),
+            m.unwrap()
+        );
+    }
+
+    #[test]
+    fn parses_insert_when_key_is_formed_of_equal_sings() {
+        let m = Message::new("foo===".to_string());
+        assert_eq!(
+            Message::Insert(InsertMessage {
+                key: "foo".to_string(),
+                value: "==".to_string()
+            }),
+            m.unwrap()
+        );
+    }
+
+    #[test]
+    fn parses_insert_with_empty_key() {
+        let m = Message::new("=foo".to_string());
+        assert_eq!(
+            Message::Insert(InsertMessage {
+                key: "".to_string(),
+                value: "foo".to_string()
+            }),
+            m.unwrap()
+        );
+    }
+
+    #[test]
+    fn parses_version() {
+        let m = Message::new("version".to_string());
+        assert_eq!(Message::Version, m.unwrap());
+    }
+
+    #[test]
+    fn parses_retrieve() {
+        let m = Message::new("foo".to_string());
+        assert_eq!(
+            Message::Retrieve(RetrieveMessage {
+                key: "foo".to_string()
+            }),
+            m.unwrap()
+        );
+    }
 }
